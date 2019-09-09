a5dcdae4f1c6ed2b40490a1076ca271947d6f608
Elgar Teo <elgarteo95@gmail.com>
2019-09-09 17:16:58 +0800

    Add Chinese graphs

--- sentiment_timetrend.R
+++ sentiment_timetrend.R
@@ -29,14 +29,17 @@ cy_sent_meeting <- cy_sent_meeting %$% .[!date == date[is.nan(positive)], ]
 
 # plot
 g_cy <- ggplot(cy_sent_meeting, aes(x = date, y = positive, group = camp)) +
-  geom_line(aes(linetype = camp, color = camp)) +
+  geom_line(aes(linetype = camp, color = camp), size = 1) +
   scale_color_manual(values=c("#999999", "#00C957", "#0000FF")) +
   scale_linetype_manual(values=c("twodash", "solid", "solid")) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position = "none",
-        axis.title.x = element_blank()) +
+  theme(axis.text.y= element_text(size = 12), axis.text.x = element_text(angle = 45, hjust = 1, size = 12),
+        legend.position = "none", axis.title.x = element_blank(), plot.title = element_text(size = 18)) +
   ylim(0, 1) +
-  ggtitle("CY Leung") +
   geom_point()
+
+g_cy_en <- g_cy + ylab("Proportion") + ggtitle("CY Leung")
+
+g_cy_zh <- g_cy + ylab("比例") + ggtitle("梁振英") + theme(text = element_text(family = "Heiti TC Light"))
 
 ###-----Carrie Lam-----
 cl_legco_text <- readRDS("cl_legco_text.rds")
@@ -61,19 +64,40 @@ cl_sent_meeting <- cl_sent_meeting %$% .[!date == date[is.nan(positive)], ]
 
 # plot
 g_cl <- ggplot(cl_sent_meeting, aes(x = date, y = positive, group = camp)) +
-  geom_line(aes(linetype = camp, color = camp)) +
-  scale_color_manual(values=c("#999999", "#00C957", "#0000FF")) +
-  scale_linetype_manual(values=c("twodash", "solid", "solid")) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position = c(.85, .9),
+  geom_line(aes(linetype = camp, color = camp), size = 1) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12), legend.position = c(.8, .8),
         axis.title.x = element_blank(), axis.title.y = element_blank(),
-        axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
+        axis.text.y = element_blank(), axis.ticks.y = element_blank(),
+        legend.text = element_text(size = 14), legend.title = element_text(size = 14),
+        plot.title = element_text(size = 18)) +
   ylim(0, 1) +
-  ggtitle("Carrie Lam") +
   geom_point()
 
+g_cl_en <- g_cl + ggtitle("Carrie Lam") + 
+  scale_linetype_manual(values = c("twodash", "solid", "solid")) + 
+  scale_color_manual(values = c("#999999", "#00C957", "#0000FF"))
+
+g_cl_zh <- g_cl + ggtitle("林鄭月娥") +
+  scale_linetype_manual(values = c("twodash", "solid", "solid"),
+                        labels = c("全部", "建制派", "非建制派"),
+                        name = "政治陣營") +
+  scale_color_manual(values = c("#999999", "#00C957", "#0000FF"),
+                     labels = c("全部", "建制派", "非建制派"),
+                     name = "政治陣營") +
+  theme(text = element_text(family = "Heiti TC Light"))
+
 ###-----Combine-----
-png("timetrend.png", width = 20, height = 10, units = "in", res = 600)
-grid.arrange(g_cy, g_cl, nrow = 1, bottom = "date",
-             top = textGrob("Legislators' Sentiment in CE-attending Sessions\n",
-                            gp = gpar(fontsize = 18, font = 2)))
+# English
+png("timetrend_en.png", width = 1500, height = 750, units = "px")
+grid.arrange(g_cy_en, g_cl_en, nrow = 1, bottom = "Date",
+             top = textGrob("Legislators' Postive Sentiment in CE-attended Sessions\n",
+                            gp = gpar(fontsize = 24, font = 2)))
+dev.off()
+
+# Chinese
+png("timetrend_zh.png", width = 1500, height = 750, units = "px")
+grid.arrange(g_cy_zh, g_cl_zh, nrow = 1, 
+             bottom = textGrob("日期", gp = gpar(fontfamily = "Heiti TC Light")),
+             top = textGrob("議員於行政長官出席立法會會議時表達之正面情感\n",
+                            gp = gpar(fontsize = 24, font = 2, fontfamily = "Heiti TC Light")))
 dev.off()
